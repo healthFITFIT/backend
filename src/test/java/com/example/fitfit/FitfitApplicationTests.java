92682cc37322b38281c9adb9f33093117cb07512
DaYeong <day0_ng@naver.com>
2025-01-30 22:12:19 +0900

    초기 설정 파일 추가

---
+++ src/test/java/com/example/fitfit/FitfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
